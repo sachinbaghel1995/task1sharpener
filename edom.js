dd486740338b5ca8733243e483ec289baf79fd94
Sachin Baghel <baghel.sachin1996@gmail.com>
2023-01-16 02:01:40 +0530

    new commit

--- edom.js
+++ edom.js
@@ -11,22 +11,23 @@
 // element=document.forms
 // console.log(element)
 
-let elem = document.getElementById('header-title')
-console.log(elem)
-elem.innerText='hello'
-elem.textContent = 'goodbye'
+// let elem = document.getElementById('header-title')
+// console.log(elem)
+// elem.innerText='hello'
+// elem.textContent = 'goodbye'
 
-elem.innerHTML = '<h3>this is html</h3>'
-// elem.style.borderBottom = 'solid 4px red'
-let elem2=document.querySelector('#main-header')
-elem2.style.borderBottom = 'solid 4px red'
+// elem.innerHTML = '<h3>this is html</h3>'
+// // elem.style.borderBottom = 'solid 4px red'
+// let elem2=document.querySelector('#main-header')
+// elem2.style.borderBottom = 'solid 4px red'
 
-let items =document.getElementsByClassName('list-group-item')
-items[1].textContent='hello'
-items[0].style.backgroundColor = 'green'
-items[0].style.fontWeight='bold'
+// let items =document.getElementsByClassName('list-group-item')
+// items[1].textContent='hello'
+// items[0].style.backgroundColor = 'green'
+// items[0].style.fontWeight='bold'
 
-for(let i=0;i<items.length;i++)
-{
-    items[i].style.backgroundColor='pink'
-}
+// for(let i=0;i<items.length;i++)
+// {
+//     items[i].style.backgroundColor='pink'
+// }
+
---
+++ edom2.js
@@ -0,0 +1,7 @@
+let element = document.getElementsByClassName('list-group-item')
+element[2].style.backgroundColor = 'green'
+
+for(let i=0;i<element.length;i++)
+{
+    element[i].style.fontWeight='bold'
+}
