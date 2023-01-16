eea43fd45d7c2c374abb0919ea831132966a042c
Sachin Baghel <baghel.sachin1996@gmail.com>
2023-01-16 10:21:11 +0530

    another branch

--- edom2.js
+++ edom2.js
@@ -1,7 +1,6 @@
-let element = document.getElementsByClassName('list-group-item')
-element[2].style.backgroundColor = 'green'
+let elem = document.getElementsByTagName('li')
+elem[4].style.backgroundColor = 'yellow'
+elem[4].innerText = 'this is new item'
 
-for(let i=0;i<element.length;i++)
-{
-    element[i].style.fontWeight='bold'
-}
+let elem2 = document.getElementsByClassName('differentclass')
+elem2.style.backgroundColor = 'red'
